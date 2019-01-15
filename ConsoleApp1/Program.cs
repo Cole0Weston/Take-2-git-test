8bae7949c4bd510776e5e42702c2c1b929fb949d
Cole Weston <colmwest@ut.utm.edu>
2019-01-15 14:55:25 -0600

    Tracking your progress: Diffs and commits.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
-            Console.WriteLine("goodbye cruel world");
+            Random rnd = new Random();
+            int number = rnd.Next(0, 100);
+            int count = 0;
+            int guess = -1;
+            Console.WriteLine("I'm thinking of a number between 0 and 100");
+            while (guess != number)
+            {
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess < number)
+                    Console.WriteLine("too low");
+                if (guess > number)
+                    Console.WriteLine("too high");
+                if (guess == number)
+                    Console.WriteLine("correct value!");
+                count++;
+            }
+            Console.WriteLine("Number of guesses made: " + count);
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
